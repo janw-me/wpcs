5a67cd613fdc432a16184db69fe8ce79a00d3243
Jan Willem Oostendorp <webmaster@janw.me>
2023-09-15 20:12:04 +0200

    added both static and regular method

--- wpcs.php
+++ wpcs.php
@@ -5,10 +5,16 @@ function wpcs_esc_str_rev( $str ) {
 }
 
 class Helpers {
-	public static function wpcs_esc_str_rev( $str ) {
+	public static function wpcs_static_esc_str_rev( $str ) {
+		return esc_html( strrev( $str ) );
+	}
+
+	public function wpcs_method_esc_str_rev( $str ) {
 		return esc_html( strrev( $str ) );
 	}
 }
 
 echo wpcs_esc_str_rev( 'Hello World!' );
-echo Helpers::wpcs_esc_str_rev( 'Hello World!' );
+echo Helpers::wpcs_static_esc_str_rev( 'Hello World!' );
+$wpcs_helper = new Helpers();
+echo $wpcs_helper->wpcs_method_esc_str_rev( 'Hello World!' );
